527bbafdb41f3aac99a55bf7c4b23a0ac4de0cc6
Adam Pingel <pingel@gmail.com>
2022-04-17 23:44:02 -0400

    another test in TestBar

--- axle-bar/src/test/scala/TestBar.scala
+++ axle-bar/src/test/scala/TestBar.scala
@@ -4,3 +4,7 @@ import org.junit.Assert.*
 class TestBar:
   @Test def t1(): Unit = 
     assertEquals("I was compiled by Scala 3. :)", msg)
+
+  @Test def t2(): Unit =
+    val bar = Bar("Cheers", 10)
+    assertEquals(bar.name, "Cheers")
